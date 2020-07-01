8cd3c03ad61ea6fc69492a699be4d619250b31d4
root <hankai17@126.com>
2020-07-01 14:17:18 +0800

    reformat

---
+++ 2.3list_del_mid/LinkList.h
@@ -0,0 +1,31 @@
+#ifndef _LINKLIST_H
+#define _LINKLIST_H
+#include<stdlib.h>
+#include<stdio.h>
+
+typedef struct _LINKNODE
+{
+  void *data;
+  struct _LINKNODE*next;
+}LinkNode; 
+
+typedef void* LinkList;
+typedef void(DATA_PRINT)(void*);
+
+LinkList Init_LinkList();
+void Insert_LinkList(LinkList list,int pos,void *data);
+
+void RemoveByPos_LinkList(LinkList list,int pos);
+
+void *Get_LinkList(LinkList list,int pos);
+
+int Size_LinkList(LinkList list);
+
+//void Print_LinkList(LinkList list,)
+void Print_LinkList(LinkList list, DATA_PRINT print);
+
+void Destroy_LinkList(LinkList list);
+
+void RemoveMiddle_LinkList(LinkList list);
+
+#endif
