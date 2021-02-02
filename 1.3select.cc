142e0a30b5bf8f89f7918e7bb2b2240ea47ee5c1
root <hankai17@126.com>
2021-02-02 03:12:21 -0800

    add order alg

---
+++ 1.3select.cc
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#define MAX 10
+
+void printArray(int* arr, int len) {
+  for (int i = 0; i < len; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
+
+void myswap(int* arr, int i, int j) {
+  int temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+//Ñ¡ÔñÅÅÐò ÉýÐò
+void select_sort(int arr[],int len) {
+  for (int i = 0; i < len; i++) {
+    int min = i;
+    for (int j = i; j < len; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min != i) {
+      myswap(arr, min, i);
+    }
+  }
+}
+
+void select_sort1(int arr[], int len) {
+    for (int i = 0; i < len; i++) {
+        for (int j = i; j < len; j++) {
+            if (arr[i] > arr[j]) {
+                myswap(arr, i, j); 
+            }
+        }
+    }
+}
+
+int main()
+{
+  int arr[MAX];
+  srand((unsigned int)time(NULL));
+  for (int i = 0; i < MAX; i++) {
+    arr[i] = rand() % MAX;
+  }
+  printArray(arr, MAX);
+  //select_sort(arr, MAX);
+  select_sort1(arr, MAX);
+  printArray(arr, MAX);
+  return EXIT_SUCCESS;
+}
