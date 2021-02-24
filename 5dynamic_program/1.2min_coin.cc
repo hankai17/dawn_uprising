29405e99c16115c833897947a041af295b584a03
root <hankai17@126.com>
2021-02-24 07:21:26 -0800

    add demo

--- 5dynamic_program/1.2min_coin.cc
+++ 5dynamic_program/1.2min_coin.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <climits>
 #include <string.h>
 
 int min_coin(int arr[], int coin_obj, int aim)
@@ -36,35 +38,55 @@ int min_coin(int arr[], int coin_obj, int aim)
     return dp[n - 1][aim] != max ? dp[n - 1][aim] : -1;
 }
 
-int main()
+// dp[i] = std::min(dp[i], dp[i - cur_coin] + 1)
+int min_coin1(std::vector<int> coins, int aim)
+{
+    std::vector<int> dp(aim + 1, INT_MAX);
+    dp[0] = 0;
+    for (int i = 1; i <= aim; i++) {
+        for (int j = 0; j < coins.size(); j++) {
+            if (i >= coins[j] && dp[i - coins[j]] != INT_MAX) {
+                dp[i] = std::min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+    }
+    return dp[aim] > aim ? -1 : dp[aim]; 
+}
+
+// dp[i] = std::min(dp[i], dp[i - cur_idx^2] + 1)
+int min_num_squares(int aim)
+{
+    std::vector<int> dp(aim + 1, INT_MAX);
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= aim; i++) {
+        for (int j = 1; j * j <= i; j++) {
+            if (j * j <= i && dp[i - j*j] != INT_MAX) {
+                dp[i] = std::min(dp[i], dp[i - j*j] + 1);
+            }
+        } 
+    }
+    return dp[aim] > aim ? -1 : dp[aim];
+}
+
+int test()
 {
     int arr[] = { 5, 2, 3 };
     std::cout << "5 2 3 compose 20 min coin: " << min_coin(arr, sizeof(arr)/sizeof(arr[0]), 20) << std::endl;
     std::cout << "5 2 3 compose 10 min coin: " << min_coin(arr, sizeof(arr)/sizeof(arr[0]), 10) << std::endl;
     std::cout << "5 2 3 compose 2 min coin: " << min_coin(arr, sizeof(arr)/sizeof(arr[0]), 2) << std::endl;
     std::cout << "5 2 3 compose 6 min coin: " << min_coin(arr, sizeof(arr)/sizeof(arr[0]), 6) << std::endl;
+
+    std::vector<int> v = { 2};
+    std::cout << min_coin1(v, 3) << std::endl;
+
+    std::cout << min_num_squares(8) << std::endl;
     return 0;
 }
 
-/*
-   class Solution(object):
-    def coinChange(self, coins, amount):
-        """
-        :type coins: List[int]
-        :type amount: int
-        :rtype: int
-        """
-        res = [0 for _ in range(amount + 1)]
+int main()
+{
+    test();
+    return 0;
+}
 
-        for i in range(1, amount + 1):
-            cost = float('inf')
-            for c in coins:
-                if i - c >= 0:
-                    cost = min(cost, res[i - c] + 1)
-            res[i] = cost
-
-        if res[amount] == float('inf'):
-            return -1
-        else:
-            return res[amount]
- */
