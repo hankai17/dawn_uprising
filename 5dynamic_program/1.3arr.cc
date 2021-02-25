44836785a8bc6e0fcc43d03ed490327c724ed1a0
hankai17 <867614535@qq.com>
2021-02-25 09:25:43 +0800

    Update 1.3arr.cc

--- 5dynamic_program/1.3arr.cc
+++ 5dynamic_program/1.3arr.cc
@@ -34,7 +34,7 @@ int max_square(std::vector<std::vector<int>> matrix) {
 }
 */
 
-// max increase subseq
+// dp[i] = std::max(dp[i], dp[idx_smal_i&val_saml_i] + 1)
 int max_increase_subseq(std::vector<int> arr)
 {
     if (arr.size() <= 1) return 1;
@@ -75,70 +75,11 @@ int robot_max(std::vector<int> arr)
     return dp[arr.size() - 1];
 }
 
-// ((aa(())((
-bool isvalid(std::string s) {
-    int count = 0;
-    for (char c : s) {
-        if (c == '(') count++;
-        else if (c == ')') {
-            count--;
-            if (count < 0) return false;
-        }
-    }
-    return count == 0;
-}
-
-/*
-p: )(() ((() ()() ()((
-p1: (() )() )() )((
-p2: (() (() (() (((
-*/
-std::vector<std::string> removeInvalidParentheses(std::string s) {
-    std::vector<std::string> ans;
-    std::queue<std::string> q;
-    q.push(s);
-    std::unordered_set<std::string> visited;
-    visited.insert(s);
-    bool found = false;
-
-    while (!q.empty()) {
-        for (int i = q.size(); i > 0; i--) {
-            std::string temp = q.front();
-            q.pop();
-            if (isvalid(temp)) {
-                found = true;
-                std::cout << "ans: " << temp << std::endl;
-                ans.push_back(temp);
-            }
-
-            for (int i = 0; i < temp.size(); i++) {
-                if (temp[i] != '(' && temp[i] != ')')
-                    continue;
-                std::string str = temp.substr(0, i) + temp.substr(i + 1);
-                std::cout << str << std::endl;
-                if (!visited.count(str)) {
-                    q.push(str);
-                    visited.insert(str);
-                }
-            }
-        }
-        if(found) break;
-    }
-    for (int i = 0; i < ans.size(); i++) {
-        std::cout << ans[i] << std::endl;
-    }
-    return ans;
-}
-
 int test()
 {
     std::vector<int> v = { 10,9,2,18,100 };
     //std::cout << max_increase_subseq(v) << std::endl;
     //std::cout << robot_max(v) << std::endl;
-    std::string s = "123456";
-    std::cout << s.substr(2) << std::endl;
-    //std::cout << s.substr(0, 0) + s.substr(0, 3) << std::endl;
-    removeInvalidParentheses("()(()");
     return 0;
 }
 
