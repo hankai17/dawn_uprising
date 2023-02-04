102e386e784f6b33628041b9fa874e520579c66a
hankai17 <867614535@qq.com>
2023-02-03 23:12:32 -0500

    230204 practise

--- test/1sort_test.cc
+++ test/1sort_test.cc
@@ -11,37 +11,180 @@ void printArray(int arr[], int len) {
 
 void bubbleSort(int arr[], int len)
 {
-
+    for (int i = 0; i < len; i++) {
+        int flag = 0;
+        for (int j = 0; j < len - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                flag = 1;
+                int tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+        if (!flag) break;
+    }
 }
 
 void insertSort(int arr[], int len)
 {
-
+    for (int i = 1; i < len; i++) {
+        if (arr[i] < arr[i - 1]) {
+            int j = i - 1;
+#if 0
+            int tmp = arr[i];
+            for (; j >= 0; j--) {
+                if (arr[j] > tmp) {
+                    arr[j + 1] = arr[j];
+                    if (j == 0) {
+                        arr[0] = tmp;
+                    }
+                } else {
+                    arr[j + 1] = tmp;
+                    break;
+                }
+            }
+#else
+            int tmp = arr[i];
+            for (; j >= 0 && arr[j] > tmp; j--) {
+                arr[j + 1] = arr[j];
+            }
+            arr[j + 1] = tmp;
+#endif
+        }
+    }
 }
 
 void selectSort(int arr[], int len)
 {
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] > arr[j]) {
+                int tmp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = tmp;
+            }
+        }
+    }
+}
 
+static void merge(int arr[], int start, int mid, int end, int tmpSpace[])
+{
+    int is = start;
+    int ie = mid;
+    int js = mid + 1;
+    int je = end;
+    int len = 0;
+    while (is <= ie && js <= je) {
+        if (arr[is] < arr[js]) {
+            tmpSpace[len++] = arr[is++];
+        } else {
+            tmpSpace[len++] = arr[js++];
+        }
+    }
+    while (is <= ie) {
+        tmpSpace[len++] = arr[is++];
+    }
+    while (js <= je) {
+        tmpSpace[len++] = arr[js++];
+    }
+    for (int i = 0; i < len; i++) {
+        arr[start + i] = tmpSpace[i];
+    }
 }
 
 void mergeSort(int arr[], int start, int end, int tmpSpace[])
 {
-
+    if (start == end) return;
+    int m = start + (end - start) / 2;
+    mergeSort(arr, start, m, tmpSpace);
+    mergeSort(arr, m + 1, end, tmpSpace);
+    merge(arr, start, m, end, tmpSpace);
 }
 
 void quickSort(int arr[], int start, int end)
 {
+    int i = start;
+    int j = end;
+    if (i < j) {
+        int base = arr[i];
+        while (i < j) {
+            while (i < j && arr[j] > base) {
+                j--;
+            }
+            if (i < j) {
+                arr[i] = arr[j];
+                i++;
+            }
+            while (i < j && arr[i] < base) {
+                i++;
+            }
+            if (i < j) {
+                arr[j] = arr[i];
+                j--;
+            }
+        }
+        arr[i] = base;
+        quickSort(arr, start, i - 1);
+        quickSort(arr, i + 1, end);
+    }
+}
 
+static void myswap(int arr[], int i, int j)
+{
+    int tmp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = tmp;
+}
+
+static void heap_adjust(int arr[], int idx, int len)
+{
+    int l = idx * 2 + 1;
+    int r = idx * 2 + 2;
+    int max_idx = idx;
+    //if (r < len) {
+    if (idx < len / 2) {
+        if (l < len && arr[max_idx] < arr[l]) {
+            max_idx = l;
+        }
+        if (r < len && arr[max_idx] < arr[r]) {
+            max_idx = r;
+        }
+        if (max_idx != idx) {
+            myswap(arr, idx, max_idx);
+            heap_adjust(arr, max_idx, len);
+        }
+    }
 }
 
 void heapSort(int arr[], int len)
 {
-
+    int m = len / 2 + 1;
+    for (int i = m; i >= 0 ; i--) {
+        heap_adjust(arr, i, len);
+    }
+    for (int i = 0; i < len; i++) {
+        myswap(arr, 0, len - i - 1);
+        heap_adjust(arr, 0, len - i - 1);
+    }
 }
 
 int bsearch(int arr[], int len, int value)
 {
-
+    int l = 0;
+    int r = len - 1;
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+        if (arr[m] == value) {
+            std::cout << "found" << std::endl;
+            return 1;
+        } else if (arr[m] < value) {
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+    }
+    std::cout << "not found" << std::endl;
+    return 0;
 }
 
 int main()
@@ -53,8 +196,17 @@ int main()
 		arr[i] = rand() % MAX;
 	}
 	printArray(arr, MAX);
+    //bubbleSort(arr, MAX);
+    //insertSort(arr, MAX);
+    //selectSort(arr, MAX);
+    //mergeSort(arr, 0, MAX - 1, tmp_arr);
+    //quickSort(arr, 0, MAX - 1);
+    heapSort(arr, MAX);
+    //bsearch(arr, MAX, 5);
+	printArray(arr, MAX);
 
     return 0;
 }
 
 // TIME ELAPSE: 20230203 
+// TIME ELAPSE: 20230204 39min 
--- test/2list_test.cc
+++ test/2list_test.cc
@@ -39,39 +39,158 @@ list_node* del_back(list_node *l, int b)
 
 list_node* del_mid(list_node *l)
 {
+    list_node *slow = l;
+    list_node *fast = l->next->next;
+    while (fast->next && fast->next->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    std::cout << "pre mid: " << slow->data << std::endl;
+    slow->next = slow->next->next;
+    return l;
 }
 
 int palindrome_list(list_node* l)
 {
+    list_node *slow = l;
+    list_node *fast = l;
+    while (fast->next && fast->next->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    std::cout << "mid: " << slow->data << std::endl;
+
+    list_node *rh = slow->next;
+    slow->next = NULL;
+
+    list_node *pre = NULL;
+    list_node *next;
+    while (rh) {
+        next = rh->next;
+        rh->next = pre;
+
+        pre = rh;
+        rh = next;
+    }
+
+    while (l && pre) {
+        if (l->data != pre->data) {
+            return 0;
+        }
+        l = l->next;
+        pre = pre->next;
+    }
+    return 1;
 }
 
 list_node *merge_lr_list(list_node *head)
 {
+    list_node *slow = head;
+    list_node *fast = head;
+    while (fast->next && fast->next->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    std::cout << "mid: " << slow->data << std::endl;
+
+    list_node *rh = slow->next;
+    slow->next = NULL;
+
+    list_node n;
+    list_node *tmp = &n;
+    while (head && rh) {
+        tmp->next = head;
+        tmp = head;
+        head = head->next;
+
+        tmp->next = rh;
+        tmp = rh;
+        rh = rh->next;
+    }
+    tmp->next = rh ? rh : head;
+    return n.next;
 }
 
 list_node *list_del_dup(list_node *l)
 {
+    if (l == NULL || l->next == NULL) return l;
+    l->next = list_del_dup(l->next);
+    return l->data == l->next->data ? l->next : l;
 }
 
 list_node *list_del_val(list_node *l1, int val)
 {
-}
-
-list_node* reverse(list_node *l)
-{
+    while (l1->data == val) {
+        l1 = l1->next;
+    }
+    list_node *h = l1;
+    list_node *pre = l1;
+    list_node *cur = l1->next;
+    while (cur) {
+        if (cur->data != val) {
+            pre = cur;
+        } else {
+            pre->next = cur->next;
+        }
+        cur = cur->next;
+    }
+    return h;
 }
 
 list_node* resign(list_node *left, list_node* start, 
         list_node* end, list_node* right)
 {
+    list_node *pre = NULL;
+    list_node *next;
+    list_node *h = start;
+    while (h != right) {
+        next = h->next;
+        h->next = pre;
+        pre = h;
+        h = next;
+    }
+    if (left) left->next = pre;
+    start->next = right;
+    return left ? left : pre;
 }
 
 void joself_list()
 {
 }
 
-void list_copy(list_node *l)
+list_node* list_copy(list_node *l)
 {
+    list_node *cur = l;
+    while (cur) {
+        list_node* n = new list_node;
+        n->data = cur->data;
+
+        list_node* next = cur->next;
+        cur->next = n;
+        n->next = next;
+        cur = next;
+    }
+    cur = l;
+    while (cur) {
+        list_node *n = cur->next;
+        if (cur->rand) {
+            n->rand = cur->rand->next;
+        }
+        cur = n->next;
+    }
+    cur = l;
+    show_list_node(l);
+    list_node node;
+    list_node *tmp = &node;
+    while (cur) {
+        list_node *n = cur->next;
+        tmp->next = n;
+        tmp = n;
+
+        cur->next = n->next;
+        cur = cur->next;
+    }
+    return node.next;
 }
 
 list_node *select_order(list_node *head)
@@ -84,6 +203,25 @@ list_node *merge_ordered_list(list_node *l1, list_node* l2)
 
 list_node *reverse_every_partion_list(list_node *head, int k)
 {
+    list_node *left = NULL;
+    list_node *start = NULL;
+    list_node *cur = head;
+    list_node *next;
+    list_node *n_h = NULL;
+    int i = 1;
+    while (cur) {
+        next = cur->next;
+        if (i == k) {
+            n_h = n_h ? n_h : cur;
+            start = start ? left->next : head; 
+            resign(left, start, cur, next);
+            left = start; 
+            i = 0;
+        }
+        i++;
+        cur = next;
+    }
+    return n_h;
 }
 
 int main()
@@ -91,16 +229,26 @@ int main()
     list_node l1; l1.data = 1;
     list_node l2; l2.data = 3;
     list_node l3; l3.data = 5;
-    list_node l4; l4.data = 7;
-    list_node l5; l5.data = 9;
-    list_node l6; l6.data = 10;
+    list_node l4; l4.data = 3;
+    list_node l5; l5.data = 1;
+    list_node l6; l6.data = 1;
 
     l1.next = &l2; l2.next = &l3;
     l3.next = &l4; l4.next = &l5;
-    l5.next = &l6; l6.next = NULL;
+    l5.next = NULL; //&l6; l6.next = NULL;
+    list_node* tmp;
 
     show_list_node(&l1);
-    show_list_node(&l1);
+    //tmp = del_mid(&l1);
+    //int ret = palindrome_list(&l1);
+    //std::cout << "ret: " << ret << std::endl;
+    //tmp = merge_lr_list(&l1);
+    //tmp = list_del_val(&l1, 1);
+    //tmp = list_copy(&l1); ///////////////////////////
+    tmp = reverse_every_partion_list(&l1, 2);
+    show_list_node(tmp);
 
     return 0;
 }
+
+// TIME ELAPSE: 20230204 48min 
